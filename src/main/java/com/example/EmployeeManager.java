26f9870b2417ed1e424bf6d45181289512a21483
Causality <m.uddner@gmail.com>
2021-03-20 12:10:01 +0100

    Add implementation testing Step 1

--- src/main/java/com/example/EmployeeManager.java
+++ src/main/java/com/example/EmployeeManager.java
@@ -4,26 +4,43 @@ import java.util.List;
 
 public class EmployeeManager {
 
-	private final EmployeeRepository employeeRepository;
-	private final BankService bankService;
+    private final EmployeeRepository employeeRepository;
+    private final BankService bankService;
 
-	public EmployeeManager(EmployeeRepository employeeRepository, BankService bankService) {
-		this.employeeRepository = employeeRepository;
-		this.bankService = bankService;
-	}
+    public EmployeeManager(EmployeeRepository employeeRepository, BankService bankService) {
+        this.employeeRepository = employeeRepository;
+        this.bankService = bankService;
+    }
 
-	public int payEmployees() {
-		List<Employee> employees = employeeRepository.findAll();
-		int payments = 0;
-		for (Employee employee : employees) {
-			try {
-				bankService.pay(employee.getId(), employee.getSalary());
-				employee.setPaid(true);
-				payments++;
-			} catch (RuntimeException e) {
-				employee.setPaid(false);
-			}
-		}
-		return payments;
-	}
+    public int payEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        int payments = 0;
+        for (Employee employee : employees) {
+            try {
+                bankService.pay(employee.getId(), employee.getSalary());
+                employee.setPaid(true);
+                payments++;
+            } catch (RuntimeException e) {
+                employee.setPaid(false);
+            }
+        }
+        return payments;
+    }
+
+    public static int add(String n1, String n2) {
+
+
+        if (n1.isEmpty() && n2.isEmpty()) {
+            return 0;
+
+        } else if (n1.isEmpty()) {
+
+            return Integer.parseInt(n2);
+        } else if (n2.isEmpty()) {
+
+            return Integer.parseInt(n1);
+        }
+
+        return Integer.parseInt(n1) + Integer.parseInt(n2);
+    }
 }
--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -1,5 +1,44 @@
 package com.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class EmployeeManagerTest {
 
+
+    @Test
+    void addTest() {
+
+        int actual = EmployeeManager.add("1", "1");
+
+        assertEquals(2, actual);
+
+    }
+
+    @Test
+    void addEmptyTest() {
+
+        int actual = EmployeeManager.add("", "");
+
+        assertEquals(0, actual);
+
+    }
+
+    @Test
+    void oneEmptyTest() {
+
+        int actual = EmployeeManager.add("", "1");
+
+        assertEquals(1, actual);
+
+    }
+
+    @Test
+    void num2EmptyTest() {
+
+        int actual = EmployeeManager.add("1", "");
+
+        assertEquals(1, actual);
+    }
 }
